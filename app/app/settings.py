2655967efcd7afbb9eecc3a53aa1d05525b3639a
Debra <dsparr1010@gmail.com>
2020-07-22 12:50:27 -0500

    added custom user model with tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party
+    
+    #Local
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
+
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """ Test creating a new user with an email is successful """
+        email = 'test@gmail.com'
+        password = 'some_pass'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password), password)
+
+    def test_new_user_email_normalized(self):
+        """Test that the email for new user is normalized"""
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='some_pass'
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test create new user without an email raises an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'some_pass')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser('test@gmail.com', 'some_pass')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
